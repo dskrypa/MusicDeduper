cc9c0babfa79176ea9554993b4fa8aba32e42d1e
dskrypa <dskrypa@gmail.com>
2016-01-01 14:04:27 -0500

    added runtime calculation

--- dedupe.py
+++ dedupe.py
@@ -3,13 +3,14 @@
 '''
 Author: Douglas Skrypa
 Date: 2015.12.30
-Version: 2
+Version: 2b
 '''
 
 import os;
 from pydub import AudioSegment;
 import hashlib;
 import shutil;
+import time;
 
 debugMode = True;
 
@@ -17,10 +18,14 @@ def main():
 	sdir = "/home/user/temp/src/";
 	ddir = "/home/user/temp/dest/";
 	
+	stime = time.perf_counter();
+	
 	fnames = os.listdir(sdir);									#Array of file names (relative path)
 	hashes = {};												#Dictionary of hashes found
 	
 	c = 0;
+	copied = 0;
+	skipped = 0;
 	k = len(fnames);
 	ksl = len(str(k));
 	fmt = "{} [{:" + str(ksl) + "d}/" + str(k) + "] {} {}";
@@ -33,13 +38,19 @@ def main():
 		fhash = hashlib.md5(sound.raw_data).hexdigest();
 		
 		if fhash not in hashes:
+			copied += 1;
 			hashes[fhash] = True;
 			shutil.copy(fpath, ddir);
 			dbg(fmt.format(fhash, c, "Copying  ", fpath));
 		else:
+			skipped += 1;
 			dbg(fmt.format(fhash, c, "Skipping ", fpath));
 		#/if
 	#/for
+	
+	etime = time.perf_counter();
+	rtime = etime-stime;
+	print("Runtime: " + time.strftime("%H:%M:%S",time.gmtime(rtime)));
 #/main
 
 def dbg(str):
